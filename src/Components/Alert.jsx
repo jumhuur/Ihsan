0f2721d8cdd0e3e51e55bb6dcd88932fe349bbe2
jumhuur <jumhuur123@hotmail.com>
2023-04-15 05:40:28 +0100

    15/04/2023

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,7 +1,50 @@
 function Alert(){
     return (
         <>
-        
+        <div className="over_alert active">
+            <div className="haye">
+                <div className="in_haye_alert">
+                {/* <div className="alert">
+                    <div className="load_qoraal">
+                        <div className="load">
+                            <div class="sk-chase">
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            </div>
+                        </div>
+                        <div className="qoraal">
+                             <h2>Fadlan Telefankaaga eeg ... </h2>
+                        </div>
+                    </div>
+                </div> */}
+                {/* <div className="alert">
+                    <div className="load_qoraal">
+                        <div className="load Qalad">
+                           <i className="fa-solid fa-ban fa-beat"></i>
+                        </div>
+                        <div className="qoraal">
+                             <h2 className="qalad">Waa qalad Lanbarku! </h2>
+                        </div>
+                    </div>
+                </div> */}
+                <div className="alert">
+                    <div className="load_qoraal">
+                        <div className="load Sax">
+                           <i className="fa-solid fa-circle-check fa-beat"></i>
+                        </div>
+                        <div className="qoraal">
+                             <h2 className="Sax">Waxaad ku Tabarucday 22$! </h2>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
         </>
     )
 }
